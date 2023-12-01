cdbe5b4f6a3f3815c82d5c9a02a033e66b4af582
mejrakucevic <mejrakucevic420@gmail.com>
2023-12-01 01:00:15 +0100

    .

--- src/index.js
+++ src/index.js
@@ -49,10 +49,32 @@ const pizzaData = [
 function App() {
   return (
     <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Woo</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open yay</footer>
   );
 }
 
