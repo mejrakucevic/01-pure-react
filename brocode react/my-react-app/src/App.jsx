87507ed0dbeb30461a8754183378107c56da72ec
mejrakucevic <mejrakucevic420@gmail.com>
2024-02-03 23:46:03 +0100

    .

--- brocode react/my-react-app/src/App.jsx
+++ brocode react/my-react-app/src/App.jsx
@@ -1,7 +1,14 @@
-import Card from "./Student.jsx";
+// import Card from "./Student.jsx";
 
-function App() {
-  return <Card />;
-}
+// function App() {
+//   return (
+//     <>
+//       {" "}
+//       <Card name="John" age={24} isStudent={true} />
+//       <Card name="Jane" age={22} isStudent={false} />
+//       <Card name="Jack" age={32} isStudent={false} />
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
