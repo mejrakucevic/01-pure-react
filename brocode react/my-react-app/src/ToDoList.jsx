73bab60542b2ee6e2453a0905c99cb352fca7b92
mejrakucevic <mejrakucevic420@gmail.com>
2024-02-11 17:25:23 +0100

    .

--- brocode react/my-react-app/src/ToDoList.jsx
+++ brocode react/my-react-app/src/ToDoList.jsx
@@ -1,58 +1,19 @@
 import React, { useState } from "react";
 
 function TBR() {
-  const [books, setBooks] = useState(["Moscow Cow", "Normal People", "LAX"]);
-  const [newBook, setNewBook] = useState([]);
-
-  function handleInputChange(event) {
-    setNewBook(event.target.value);
-  }
-
-  function addTBR() {}
-
-  function deleteTBR(index) {}
-
-  function moveTBRup(index) {}
-
-  function moveTBRdown(index) {}
   return (
     <>
-      <div className="tbrList">
-        <h1>To Be Read</h1>
+      <div className="main">
+        <h1>ToDo</h1>
+        <p>Insert Task</p>
+        <textarea name="" id="inputTask" cols="30" rows="1"></textarea>
+        <button>Add</button>
+      </div>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Enter a book..."
-            value={newBook}
-            onChange={handleInputChange}
-          />
-          <button className="btn" onClick={addTBR}>
-            Add
-          </button>
-        </div>
-
-        <ol>
-          {books.map((book, index) => (
-            <li key={index}>
-              <span className="text">{book}</span>
-              <button className="deleteBtn" onClick={() => deleteTBR(index)}>
-                Delete
-              </button>
-
-              <button className="moveTBRup" onClick={() => moveTBRup(index)}>
-                ☝🏼
-              </button>
-
-              <button
-                className="moveTBRdown"
-                onClick={() => moveTBRdown(index)}
-              >
-                👇🏼
-              </button>
-            </li>
-          ))}
-        </ol>
+      <div className="taskBox">
+        <textarea name="" id="taskList" cols="30" rows="10">
+          Hello
+        </textarea>
       </div>
     </>
   );
