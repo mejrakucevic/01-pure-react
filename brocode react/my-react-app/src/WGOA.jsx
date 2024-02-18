7e7ac077da32648e7a01b257b4bbe17b444c3e2e
mejrakucevic <mejrakucevic420@gmail.com>
2024-02-18 20:05:56 +0100

    .

--- brocode react/my-react-app/src/WGOA.jsx
+++ brocode react/my-react-app/src/WGOA.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 function WGOA() {
+  const [showImage, setShowImage] = useState(false);
+
+  const handleBtnClick = () => {
+    setShowImage(true);
+  };
+
+  const alertMsg = () => {
+    alert("Yay!! Cant wait!");
+  };
   // jos onChange da se doda za btns i prikaz slike posle Yes
   return (
     <div className="box">
       <h1>Will you go out with me? :3</h1>
 
-      <img src="./pics/ask.gif" alt="cute animal" className="img1" />
+      {!showImage && (
+        <img src="./pics/ask.gif" alt="cute animal" className="img1"></img>
+      )}
+
+      {showImage && (
+        <img src="./pics/happy.gif" alt="cute animal" className="img2"></img>
+      )}
 
       <div className="buttons">
-        <button className="yesBtn">Yes</button>
+        <button className="yesBtn" onClick={handleBtnClick} {alertMsg}>
+          Yes
+        </button>
         <button className="noBtn">No</button>
       </div>
     </div>
