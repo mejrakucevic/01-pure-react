b40d4b1e184dcbedaeeb409a6e07d90072834f12
mejrakucevic <mejrakucevic420@gmail.com>
2024-02-26 16:23:46 +0100

    .

--- brocode react/my-react-app/src/Practice.jsx
+++ brocode react/my-react-app/src/Practice.jsx
@@ -13,9 +13,14 @@ function Practice() {
   // );
 
   const [attribute, setAttribute] = useState("");
+  const [attribute2, setAttribute2] = useState("");
 
   const handleClick = () => {
     setAttribute("depresed lol");
+  };
+
+  const handleClick2 = () => {
+    setAttribute2("also depressed but probably wholesome");
   };
   return (
     <>
@@ -23,6 +28,11 @@ function Practice() {
         <h1>Do you like Conan gray?</h1>
         <button onClick={handleClick}>Yes</button>
         <h2>You're...{attribute}</h2>
+        <div className="container2">
+          <h1>How about Clairo?</h1>
+          <button onClick={handleClick2}>Yes</button>
+          <h2>You're...{attribute2}</h2>
+        </div>
       </div>
     </>
   );
